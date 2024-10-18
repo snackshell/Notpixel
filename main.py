f7dcd45aaec6f71e0fd1e7340082885d93e9b8ea
paradox <solomonadonay1@gmail.com>
2024-10-18 01:23:02 -1200

    Update main.py

--- main.py
+++ main.py
@@ -254,16 +254,14 @@ def reset_proxy():
     log_message("Proxy data reset successfully", Fore.GREEN)
 
 def print_banner():
-    ascii_banner = pyfiglet.figlet_format("TIGRAY TECH", justify="center")
+    ascii_banner = pyfiglet.figlet_format("TIGRAY TECH")
     
     green_bold = "\033[1m\033[32m"
-    red_bold = "\033[1m\033[31m"
     reset = "\033[0m"
     
     print(f"{green_bold}{ascii_banner}{reset}")
-    
-    print(f"{red_bold} made and written by TIGRAY TECH{reset}")
-    print(f"{red_bold} join telegram channel: https://t.me/tigraytech{reset}")
+    print(f"{green_bold}made and written by TIGRAY TECH{reset}")
+    print(f"{green_bold}join telegram channel: https://t.me/tigraytechc{reset}")
 
 print_banner()
 
